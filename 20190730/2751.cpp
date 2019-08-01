2db7be00563173a51629ab042973decef117e8b1
yejinkim <yejin0216@gmail.com>
2019-08-01 20:19:40 +0900

    - 퀵소트를 이용한 정렬문제 풀이 (첫번째 도전!)

---
+++ 20190730/2751.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+void quickSort(int *array, int left, int right) {
+
+    int i = left;
+    int j = right;
+    int pivot = array[(left+right)/2];
+
+    while ( i <= j ) {
+        while ( array[i] < pivot ) i++;
+        while ( array[j] > pivot ) j--;
+
+        if ( i <= j ) {
+            //swap한다.
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+    if ( left < j ) {
+        quickSort(array, left, j);
+    }
+    if ( right > i ) {
+        quickSort(array, i, right);
+    }
+
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int *array = new int[n]; //입력받을 배열
+    for ( int i=0; i<n; i++ ) {
+        scanf("%d", &array[i]);
+    }
+
+    quickSort(array, 0, n-1);
+
+    for ( int i=0; i<n; i++ ) {
+        printf("%d", array[i]);
+    }
+
+    return 0;
+}
